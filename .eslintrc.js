03f315ce2def96d35d164d04f65bc9a26c558b3f
Aleksei Kharitonov <northdell@gmail.com>
2018-08-28 16:50:10 +0300

    отменил автоисправление кавычек

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,10 @@ module.exports = {
         ],
         "quotes": [
             "error",
-            "single"
+            "single",
+             {
+                 "avoidEscape": true
+             }
         ],
         "semi": [
             "error",
